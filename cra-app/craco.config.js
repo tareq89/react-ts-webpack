3e4ff81cc26b8877faa81a8d3696e0ddce311ea8
tahmidrahman-dsi <tahmidrahman.dsi@gmail.com>
2020-07-21 17:43:55 +0600

    Support for jest module resolution

--- cra-app/craco.config.js
+++ cra-app/craco.config.js
@@ -1,28 +1,35 @@
 /* craco.config.js */
-const path = require("path");
+const path = require("path")
 module.exports = function ({ env }) {
-	return {
-		webpack: {
-			alias: {
-				"~": path.resolve(__dirname, "src"),
-			},
-		},
-		style: {
-			sass: {
-				loaderOptions: (sassLoaderOptions, { env, paths }) => {
-					sassLoaderOptions = {
-						...sassLoaderOptions,
-						prependData: "@import './src/sass/global/index.scss';", // it can be a function to load sass data from environment
-						implementation: require("node-sass"),
-						sourceMap: true,
-						sassOptions: {
-							indentWidth: 2,
-							outputStyle: "compressed",
-						},
-					};
-					return sassLoaderOptions;
-				},
-			},
-		},
-	};
-};
+  return {
+    webpack: {
+      alias: {
+        "~": path.resolve(__dirname, "src")
+      }
+    },
+    style: {
+      sass: {
+        loaderOptions: (sassLoaderOptions, { env, paths }) => {
+          sassLoaderOptions = {
+            ...sassLoaderOptions,
+            prependData: "@import './src/sass/global/index.scss';", // it can be a function to load sass data from environment
+            implementation: require("node-sass"),
+            sourceMap: true,
+            sassOptions: {
+              indentWidth: 2,
+              outputStyle: "compressed"
+            }
+          }
+          return sassLoaderOptions
+        }
+      }
+    },
+    jest: {
+      configure: {
+        moduleNameMapper: {
+          "^~(.*)$": "<rootDir>/src/$1"
+        }
+      }
+    }
+  }
+}
